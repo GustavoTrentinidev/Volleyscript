edb187f300c022770897d957ea7cc7ece606b33e
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-07-17 16:28:18 -0300

    Troca básica de telas

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ quadraImage.src = "./images/quadrapadrao.jpg"
 const bolaImage = new Image()
 bolaImage.src = "./images/bola.png"
 
+const telaDeInicio = new Image()
+telaDeInicio.src = "./images/telainicio.png"
+
 class Objeto{
     constructor(imagem,x,y,w,h){
         this.imagem = imagem
@@ -43,13 +46,6 @@ posicao4Esquerda = new PosicaoJogadores(bolaImage, 650, 200, 100, 100,'l')
 posicao3Esquerda = new PosicaoJogadores(bolaImage, 750, 450, 100, 100,'l')
 posicao2Esquerda = new PosicaoJogadores(bolaImage, 650, 700, 100, 100,'l')
 
-// posicao1Direita = new PosicaoJogadores(bolaImage, 1500, 700, 100, 100, 'r')
-// posicao6Direita = new PosicaoJogadores(bolaImage, 1300, 450, 100, 100, 'r')
-// posicao5Direita = new PosicaoJogadores(bolaImage, 1500, 200, 100, 100, 'r')
-// posicao4Direita = new PosicaoJogadores(bolaImage, 1050, 700, 100, 100, 'r')
-// posicao3Direita = new PosicaoJogadores(bolaImage, 950, 450, 100, 100, 'r')
-// posicao2Direita = new PosicaoJogadores(bolaImage, 650, 700, 100, 100, 'r')
-
 posicao1Direita = new PosicaoJogadores(bolaImage, 1500, 200, 100, 100, 'r')
 posicao6Direita = new PosicaoJogadores(bolaImage, 1300, 450, 100, 100, 'r')
 posicao5Direita = new PosicaoJogadores(bolaImage, 1500, 700, 100, 100, 'r')
@@ -70,9 +66,9 @@ class ObjetoBola extends Objeto{
     atualizaPosicao(req){
         if(this.x != req.x){
             if(this.x < req.x){
-                this.x = this.x + 15
+                this.x = this.x + 10
             }else{
-                this.x = this.x - 15
+                this.x = this.x - 10
             }
         }
         if(this.y != req.y){
@@ -88,9 +84,10 @@ class ObjetoBola extends Objeto{
 quadra = new Objeto(quadraImage,0,0,1800,1000)
 // Área do jogo começa realmente em x: 125 e y: 105
 // Temina em x: 1545 e y: 745
+telaComeçar = new Objeto(telaDeInicio,0,0,1800,1000)
 bola = new ObjetoBola(bolaImage, posicao1Direita.x, posicao1Direita.y, 130, 155)
 
-function fazColisao(bola, posicao){
+function tocouNaBola(bola, posicao){
     if(bola.x == posicao.x && bola.y == posicao.y){
         console.log('Recebeu!!!!')
     }
@@ -98,13 +95,41 @@ function fazColisao(bola, posicao){
 
 
 function gameLoop(){
-    quadra.renderSelf()
-    bola.renderSelf()
-    posicoes.forEach(( posicao )=>{
-        posicao.renderSelf()
-    })
-    fazColisao(bola, posicao6Esquerda)
+    tocouNaBola(bola, posicao6Esquerda)
     bola.atualizaPosicao({x: posicao1Esquerda.x, y: posicao1Esquerda.y})
+    showScreen.renderSelf()
     requestAnimationFrame(gameLoop)
 }
+
+
+let showScreen = {}
+function changeScreen(screen){
+    showScreen = screen
+}
+const screens = {
+    startScreen:{
+        renderSelf(){
+            quadra.renderSelf()
+            telaComeçar.renderSelf()
+        }
+    },
+    gameScreen:{
+        renderSelf(){
+            quadra.renderSelf()
+            bola.renderSelf()
+            posicoes.forEach(( posicao )=>{
+                posicao.renderSelf()
+            })
+        }
+    }
+}
+
+
+var eventos = ["keydown" , "click"]
+eventos.forEach((evento) => {
+    window.addEventListener(evento, ()=>{
+        changeScreen(screens.gameScreen)
+    })
+})
+changeScreen(screens.startScreen)
 gameLoop()
